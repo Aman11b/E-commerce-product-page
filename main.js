b14ca472cd07666841ce7a66dc3a9b422d0be472
Aman Singh <amansingh1100b@gmail.com>
2025-06-16 18:59:58 +0000

    added the bubble text over cart icon

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("hi")
+const addToCart = document.querySelector(".add-to-cart");
+const cartContainer = document.querySelector(".cart-container");
+
+let count = 0;
+
+addToCart.addEventListener("click", () => {
+  count++;
+  cartContainer.setAttribute("data-count", count);
+});
