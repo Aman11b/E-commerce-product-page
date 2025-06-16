983f3054b8d64e865de84131d8e9531bc730d6ea
Aman Singh <amansingh1100b@gmail.com>
2025-06-16 19:29:48 +0000

    added increase decrease functionality

--- main.js
+++ main.js
@@ -7,3 +7,19 @@ addToCart.addEventListener("click", () => {
   count++;
   cartContainer.setAttribute("data-count", count);
 });
+
+const decreaseBtn = document.querySelector(".decrease-btn");
+const countOutput = document.querySelector(".count-output");
+const increaseCount = document.querySelector(".increase-btn");
+
+let outputCount = 0;
+increaseCount.addEventListener("click", () => {
+  outputCount++;
+  countOutput.textContent = outputCount;
+});
+decreaseBtn.addEventListener("click", () => {
+  if (outputCount > 0) {
+    outputCount--;
+    countOutput.textContent = outputCount;
+  }
+});
