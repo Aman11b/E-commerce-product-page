3e32b86ac1b84a71f1d445bf210f4194b5f3897e
Aman11b <amansingh1100b@gmail.com>
2025-06-18 06:08:30 +0530

    added clickable function to avatar btn

--- main.js
+++ main.js
@@ -1,22 +1,36 @@
 const addToCart = document.querySelector(".add-to-cart");
 const cartContainer = document.querySelector(".cart-container");
 
+const decreaseBtn = document.querySelector(".decrease-btn");
+const countOutput = document.querySelector(".count-output");
+const increaseCount = document.querySelector(".increase-btn");
+
+const avatar = document.querySelector(".avatar");
+const cartDropdown = document.getElementById("cart-dropdown");
+
+avatar.addEventListener("click",()=>{
+  if(cartDropdown.style.display === "none"){
+    cartDropdown.style.display="";
+  }else{
+    cartDropdown.style.display="none";
+  }
+})
+
+
 let count = 0;
+
+let outputCount = 0;
 
 addToCart.addEventListener("click", () => {
   count++;
   cartContainer.setAttribute("data-count", count);
 });
 
-const decreaseBtn = document.querySelector(".decrease-btn");
-const countOutput = document.querySelector(".count-output");
-const increaseCount = document.querySelector(".increase-btn");
-
-let outputCount = 0;
 increaseCount.addEventListener("click", () => {
   outputCount++;
   countOutput.textContent = outputCount;
 });
+
 decreaseBtn.addEventListener("click", () => {
   if (outputCount > 0) {
     outputCount--;
