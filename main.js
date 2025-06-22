99133b894160e4f2fecd15c11d4efa26ad8f7176
Aman11b <amansingh1100b@gmail.com>
2025-06-22 23:07:20 +0530

    mobile navigation completed

--- main.js
+++ main.js
@@ -33,6 +33,26 @@ const lightboxThumbnailsGallery = document.querySelectorAll(
   ".lightbox-thumbnails-gallery img"
 );
 
+const primaryNav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click",()=>{
+  const visibility = primaryNav.getAttribute("data-visible");
+  if(visibility === 'false'){
+    primaryNav.setAttribute("data-visible",true);
+    navToggle.setAttribute("aria-expanded",true);
+
+    document.body.classList.add("overlay-active");
+  }
+  else if(visibility === 'true'){
+    primaryNav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+
+    document.body.classList.remove("overlay-active");
+
+  }
+})
+
 mainImage.addEventListener("click", () => {
   openLightBox(0);
 });
